1de8dd13dbf6d93459f29e1b5b8828393c01a476
jjones2018 <34288398+jjones2018@users.noreply.github.com>
2017-12-06 01:21:29 -0800

    Update Julion.js

--- Julion.js
+++ Julion.js
@@ -1,24 +1,47 @@
-//Julion's code
+drawBackground();
+drawSnow();
+drawSnowman();
+drawGround();
+drawHouse();
 
-//high level functions
-drawBackground()
-drawSnowman()
-
-//making the background
+// fuction for making the background
 function drawBackground() {
 penDown();
 penRGB(0, 0, 237, 1);
 dot(1000);
 }
-
-//drawSnowman function
-function drawSnowman(){
-  drawBody()
-  drawFace()
-  drawButtons()
-  drawArms()
+//fucntion for the snow
+//the snow for the background. This goes before everything else
+function drawSnow() {
+  for (var i = 0; i < 190; i++) {
+    penUp();
+    moveTo (randomNumber (0, 316), randomNumber (0,450));
+    penRGB(255, 255, 255, 1);
+    penDown();
+    dot(2);
+    penUp();
+  }
 }
-//body for the snowman
+
+//drawing the ground
+function drawGround() {
+  penUp();
+  moveTo(320, 449);
+  penDown();
+  penRGB(255, 255, 255, 1);
+  penWidth(75);
+  turnRight(90);
+  moveForward(1000);
+}
+
+//drawSnowman function- high level
+function drawSnowman(){
+  drawBody();
+  drawFace();
+  drawButtons();
+}
+
+//body for the snowman-low level
 function drawBody() {
   penUp();
   moveTo (267, 397);
@@ -30,9 +53,9 @@ function drawBody() {
   moveForward (28);
   dot (15);
 }
-//face for the snowman
+
+//face for the snowman- low level
 function drawFace() {
-  //eyes
   penUp();
   moveTo (260, 336);
   penRGB (0, 0, 0, 1);
@@ -41,22 +64,83 @@ function drawFace() {
   moveTo (274, 336);
   dot (1);
   penUp();
-  //start the smiley face :)
-  moveTo(256, 343);
-  penWidth (1)
+  moveTo(260, 343);
+  penWidth (1);
   penDown();
   turnLeft(180);
-  arcLeft(180, 10);
-  
+  arcLeft(180, 8);
+  penUp();
 }
+//this is for the buttons on the snowman
 function drawButtons(){
-  //save for the desktop
+  moveTo(266, 363);
+  penRGB(0, 0, 0, 1);
+  dot(1);
+  turnLeft(180);
+    moveForward(10);
+    dot(1);
+    penUp();
+     moveForward(10);
+    dot(1);
+    penUp();
+     moveForward(10);
+    dot(1);
+    penUp();
 }
-function drawArms() {
-  
+//house -the highlevel function
+function drawHouse() {
+  drawBox()
+  drawRoof()
+  drawDoor()
 }
 
+//were going to start off with the "box"
+function drawBox() {
+  penUp();
+  moveTo(194, 395);
+  penDown();
+  turnRight(90);
+  penRGB(210, 180, 140, 1);
+  penWidth(45);
+  for (var i = 0; i < 4; i++) {
+    moveForward(100);
+    turnLeft(90);
+  }
+  penUp();
+  moveTo(145, 352);
+  dot(50);
+}
 
+//this is the low level function for a roof
+function drawRoof() {
+  penUp();
+  moveTo (73, 283);
+  penDown();
+  penRGB (128, 0, 0, 1);
+  penWidth(58);
+  turnRight (90);
+  moveForward(140);
+  turnLeft (130);
+  moveForward (95);
+  turnLeft (93);
+  moveForward (95);
+  turnLeft(137);
+  turnLeft(90);
+}
 
-
-
+//this is the low level function for the door
+function drawDoor() {
+  penUp();
+  moveTo (126, 419);
+  penRGB (0, 0, 0, 1);
+  penWidth(1);
+  penDown();
+  moveForward(50);
+  turnRight();
+  moveForward(30);
+  turnRight();
+  moveForward(50);
+  penUp();
+  moveTo (149, 399);
+  dot(2);
+}
