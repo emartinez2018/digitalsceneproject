e85f2ad3f8e1ff6d9e92d1f20ba1923e8ee3b5a7
KevinCardenas2019 <34288687+KevinCardenas2019@users.noreply.github.com>
2017-12-05 16:39:29 -0800

    Update kevin.js

--- kevin.js
+++ kevin.js
@@ -1,7 +1,19 @@
+//high level functions
+drawBackground();
+drawSnowman();
+//making the background
+function drawBackground() {
+  penRGB(0, 0, 237, 1);
+  dot(1000);
+}
 drawTree();
 function drawTree() {
+  penUp();
   for (var i = 0; i < 1; i++) {
+    moveTo(70, 331);
     penDown();
+    penRGB(0, 180, 0, 1);
+    penWidth(4);
     turnRight(90);
     moveForward(25);
     moveForward(25);
@@ -19,7 +31,8 @@ function drawTree() {
     turnLeft(120);
     moveForward(25);
     penUp();
-    moveTo(181, 264);
+    moveTo(90, 354);
+    penRGB(139, 80, 14, 1);
     penDown();
     turnLeft(150);
     moveForward(25);
@@ -28,3 +41,65 @@ function drawTree() {
     turnLeft(90);
     moveForward(25);
     penUp();
+  }
+}
+
+
+
+
+//drawSnowman function
+drawSnowman();
+function drawSnowman(){
+  drawBody();
+  drawFace();
+  drawButtons();
+  drawArms();
+}
+//body for the snowman
+function drawBody() {
+  penUp();
+  moveTo (267, 397);
+  penDown();
+  penRGB (255, 255, 255, 1);
+  dot (28);
+  moveForward (30);
+  dot (20);
+  moveForward (28);
+  dot (15);
+}
+//face for the snowman
+function drawFace() {
+  //eyes
+  penUp();
+  moveTo (260, 336);
+  penRGB (0, 0, 0, 1);
+  dot (1);
+  penUp();
+  moveTo (274, 336);
+  dot (1);
+  penUp();
+  //start the smiley face :)
+  moveTo(256, 343);
+  penWidth (1);
+  penDown();
+  turnLeft(180);
+  arcLeft(180, 10);
+  
+}
+function drawButtons(){
+  //save for the desktop
+}
+function drawArms() {
+  
+}
+//drawing the ground
+function drawGround() {
+  penUp();
+  moveTo(320, 449);
+  penDown();
+  penRGB(255, 255, 255, 1);
+  penWidth(90);
+  turnLeft(90);
+  moveForward(1000);
+}
+drawGround();
